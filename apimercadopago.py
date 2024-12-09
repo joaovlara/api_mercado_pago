85284a862baa37c0b90ab990f98b0bf1fa9e786d
Joao Lara <joaovdelara@gmail.com>
2024-12-09 10:17:42 -0300

    adiciona api mercado pago

---
+++ apimercadopago.py
@@ -0,0 +1,23 @@
+import mercadopago
+
+sdk = mercadopago.SDK("YOUR_ACCESS_TOKEN")
+
+request_options = mercadopago.config.RequestOptions()
+request_options.custom_headers = {
+    'x-idempotency-key': '<SOME_UNIQUE_VALUE>'
+}
+
+payment_data = {
+    "transaction_amount": 100,
+    "token": "CARD_TOKEN",
+    "description": "Payment description",
+    "payment_method_id": 'visa',
+    "installments": 1,
+    "payer": {
+        "email": 'test_user_123456@testuser.com'
+    }
+}
+result = sdk.payment().create(payment_data, request_options)
+payment = result["response"]
+
+print(payment)
